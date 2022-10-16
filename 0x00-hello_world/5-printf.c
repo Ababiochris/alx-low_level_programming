3fb110a82ccd279c941b0c1bb0732086b2323b8e
Ababiochris <hopeemmanuel1986@gmail.com>
2022-10-15 22:07:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *
+ * Return:0 if np errors, return non zero if errors
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
